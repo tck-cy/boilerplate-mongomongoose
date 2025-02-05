2e5bfe1fcdb8a9a46b9b2e28158f0c74fb0d1447
Cylus Tibayungwa <99850439+tck-cy@users.noreply.github.com>
2025-02-05 22:11:06 +0300

    completed tutorial fcc

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # MongoDB and Mongoose Challenges
 
 This is the boilerplate for the MongoDB and Mongoose lessons. Instructions for completing these lessons start at https://www.freecodecamp.org/learn/apis-and-microservices/mongodb-and-mongoose/
+
+
+
+MongoDB cluster Username: cylustiba
+                 Password: W1yz4Zl8PBVUWvbP
+
+
+MongoDB  URI with password only:      mongodb+srv://cylustiba:W1yz4Zl8PBVUWvbP@testcluster.lk7tb.mongodb.net/?retryWrites=true&w=majority&appName=testCluster
+
+
+
+
+
+MongoDB  URI with Database name :      mongodb+srv://cylustiba:W1yz4Zl8PBVUWvbP@testcluster.lk7tb.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=testCluster
--- myApp.js
+++ myApp.js
@@ -1,55 +1,132 @@
 require('dotenv').config();
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
-let Person;
+// Define the personSchema
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+});
+
+let Person  = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
-};
+    // Create a new document instance using the Person model
+    const person = new Person({ 
+      name: "John", 
+      age: 25, 
+      favoriteFoods: ["Pizza", "Burger"]
+     });
+     // Save the document to the database
+    person.save((err, data) => {
+      if (err) return done(err);
+      done(null, data);
+    });
+  };
+ 
+
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  // Use Model.create() to insert multiple documents
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      // If there's an error, pass it to the callback
+       return done(err);
+      }
+        // If successful, pass the saved data to the callback
+    done(null, data);
+  });
+   
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return done(err);
+    done(null , data);
+  });
+  
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+  Person.findById( personId, (err, data) => {
+    if (err) return done(err);
+    done(null , data);
+  });
+  };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return done(err);
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, updatedPerson) => {
+      if (err) return done(err);
+      done(null, updatedPerson); 
+    });
+  });
+ 
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet }, 
+    { new: true },
+    (err, updatedPerson) => {
+      if(err) return done(err);
+      done(null, updatedPerson);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if(err) return done(err);
+    done(null, removedPerson);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.deleteMany({ name: nameToRemove }, (err, result) => {
+    if(err){ return done(err);}
+    done(null, result);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+  .sort({ name: 1 })
+  .limit(2)
+  .select({ age: 0 })
+  .exec((err, data) => {
+    if(err) {
+      return done(err);
+    }
+    done(null, data);
+  })
 };
+
+console.log('Server is running Quite well');
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
